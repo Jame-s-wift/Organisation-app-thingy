e17f7ad85df0cf07c020e34083896720e83fece1
James Toh <jtyflol@gmail.com>
2023-07-12 14:35:27 +0800

    Initial Commit

---
+++ Organisation app thingy/Organisation_app_thingyApp.swift
@@ -0,0 +1,17 @@
+//
+//  Organisation_app_thingyApp.swift
+//  Organisation app thingy
+//
+//  Created by James Toh on 12/7/23.
+//
+
+import SwiftUI
+
+@main
+struct Organisation_app_thingyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
